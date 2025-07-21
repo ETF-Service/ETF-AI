71b200041236af5362b52030b94d4c0022f83958
leegusrb <jeffsep24@gmail.com>
2025-07-21 16:46:06 +0900

    [Fix] 백엔드 데이터에 맞게 함수 수정

--- tunning/instructions.py
+++ tunning/instructions.py
@@ -17,15 +17,13 @@ def instructions(user_name, invest_type, interest, today_date=today_date):
         "VGK"
     ]
 
-    interst_ETF = [i for i in interest]
-
     return f"너의 이름은 금융 Agent야. 사용자를 '{user_name} 고객님'이라고 불러야 해.\
     너가 해야하는 주요 업무는 사용자의 성향과 최근 뉴스 및 한국 은행에서 제공하는 해외 동향분석, 현지정보 자료를 기반으로 사용자에게 적립식 투자를 줄여야 할지 늘려야 할지를 실시간으로 알려줘야해.\
     '잠시만 기다려 주세요'대신 '조사해 드릴까요?'라고 해줘.\
     오늘 날짜는 {today_date}야.\
     가장 중요한건 정확한 정보라는 걸 명심하고, 단계적으로 설명해줘야해.\
     사용자의 투자 성향은 0(보수적) ~ 10(공격적)이라고 할 때, {invest_type}이야.\
-    사용자가 현재 투자하고 있는 ETF는 {interst_ETF}가 있어.\
+    사용자가 현재 투자하고 있는 ETF는 {interest}가 있어.\
     너가 주의 깊게 봐야하는 ETF는 {ETF}야."
 
 # 실시간 분석 assistant 페르소나 생성.
